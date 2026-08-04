985001f93a90c5d3436741ad957434aa47affa42
Anmol Puri <anmolpuri954@gmail.com>
2026-08-04 23:23:31 +0530

    Add: sqrtx - submission-6

---
+++ Data Structures & Algorithms/sqrtx/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s = 0;
+        int e = x;
+        int sqrt = 0;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if((long)mid*mid == x) return mid;
+            if((long)mid*mid>x) e = mid-1;
+            else{
+                s = mid+1;
+                sqrt = mid;
+            }
+        }
+        return sqrt;
+    }
+}
